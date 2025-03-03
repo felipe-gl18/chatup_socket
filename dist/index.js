e5e40f240dea16b962bf82baacc969396c82e21b
Felipe Lino <felipegadelha2004@gmail.com>
2025-03-03 10:59:09 -0300

    adds

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,12 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const node_http_1 = require("node:http");
-const socket_io_1 = require("socket.io");
-const cors_1 = __importDefault(require("cors"));
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import cors from "cors";
 const port = process.env.PORT || 3000;
-const app = (0, express_1.default)();
-app.use((0, cors_1.default)({ origin: "http://localhost:5173", methods: ["GET", "POST"] }));
-const server = (0, node_http_1.createServer)(app);
-const io = new socket_io_1.Server(server, {
+const app = express();
+app.use(cors({ origin: "http://localhost:5173", methods: ["GET", "POST"] }));
+const server = createServer(app);
+const io = new Server(server, {
     cors: {
         origin: ["http://localhost:5173"],
         methods: ["GET", "POST"],
@@ -20,17 +15,17 @@ const io = new socket_io_1.Server(server, {
 const users = {};
 io.on("connection", (socket) => {
     socket.on("register", (user) => {
-        users[user.phonenumber] = Object.assign(Object.assign({}, user), { socketID: socket.id });
+        users[user.phonenumber] = { ...user, socketID: socket.id };
         io.emit("newUser", users);
     });
     socket.on("getUsers", (phonenumber) => {
-        const usersList = Object.assign({}, users);
+        const usersList = { ...users };
         delete usersList[phonenumber];
         socket.emit("users", usersList);
     });
     socket.on("deleteUser", ({ deletingUserPhonenumber, deletedUserPhonenumber }) => {
         const socketID = users[deletingUserPhonenumber].socketID;
-        const usersList = Object.assign({}, users);
+        const usersList = { ...users };
         delete usersList[deletedUserPhonenumber];
         delete usersList[deletingUserPhonenumber];
         io.to(socketID).emit("users", usersList);
